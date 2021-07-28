3992e7ebd0120cb6592e3c7a90b662e99b6825d3
okada <6321515@ed.tus.ac.jp>
2021-07-28 19:45:47 +0900

    Option

---
+++ Assets/Scripts/Option.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Option : MonoBehaviour
+{
+	public GameObject sphere;
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			sphere.SetActive(true);
+		}
+		else if(Input.GetMouseButtonDown(1))
+		{
+			sphere.SetActive(false);
+		}
+	}
+}
