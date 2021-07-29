793365c22ebfe7e9dc1aaf59738b86a405c906e4
okada <6321515@ed.tus.ac.jp>
2021-07-29 16:11:15 +0900

    titleからoptionにシーン遷移なしで移動

---
+++ Assets/Scripts/Home_option.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Home_option : MonoBehaviour
+{
+	public GameObject Option;
+	public GameObject Home;
+	public void OnclickStartButton()
+	{
+			Option.SetActive(false);
+			Home.SetActive(true);
+	}
+}
