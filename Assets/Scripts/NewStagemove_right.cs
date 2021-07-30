bffe2a1239a210e14bf0771bb6eda31ef83c5837
okada <6321515@ed.tus.ac.jp>
2021-07-30 19:40:36 +0900

    Time,Count

--- Assets/Scripts/NewStagemove_right.cs
+++ Assets/Scripts/NewStagemove_right.cs
@@ -2,22 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Stagemove_left : MonoBehaviour
+public class NewStagemove_right : MonoBehaviour
 {
 	public RectTransform stage1;
 	public RectTransform stage2;
 	private int counter = 0;
-	//private int click = 0;
+	private int click = 0;
 	private float move = -6f;
 	bool left = false;
 
 	public void OnClick(){
+		if(click==0){
 			left = true;
+			click++;
+		}
 	}
 
 	void Update(){
-		
-		if(left==true&& stage2.position.x > 459){
+
+		if(left==true){
 			stage1.position += new Vector3(move,0,0);
 			stage2.position += new Vector3(move,0,0);
 			counter++;
@@ -27,8 +30,6 @@ public class Stagemove_left : MonoBehaviour
 				counter = 0;
 				left = false;
 			}
-		}else{
-			left = false;
 		}
 	}
 }
