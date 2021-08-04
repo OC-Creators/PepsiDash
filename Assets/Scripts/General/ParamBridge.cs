b89eb5266adf37b0c05baa7f636683217ff86583
miki <government.ocean@gmail.com>
2021-08-04 20:04:59 +0900

    Managerでシーン遷移、ビュー遷移をするように修正
    +ViewModeの列挙子を修正
    +ParamBridgeでシーンをまたぐ変数を管理

---
+++ Assets/Scripts/General/ParamBridge.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace General
+{
+    public class ParamBridge : MonoBehaviour
+    {
+        public static ViewMode prevVMode = ViewMode.Dummy;
+        private static ViewMode vmode = ViewMode.Dummy;
+        public static ViewMode VMode
+        {
+            get { return vmode; }
+            set
+            {
+                if (vmode != value)
+                {
+                    prevVMode = vmode;
+                    vmode = value;
+                    updateSignal = Signal.UpdateView;
+                }
+            }
+        }
+
+        public static ViewMode PrevVMode
+        {
+            get { return prevVMode; }
+        }
+
+        private static ScreenMode smode = ScreenMode.Dummy;
+        public static ScreenMode SMode
+        {
+            get { return smode; }
+            set
+            {
+                if (smode != value)
+                {
+                    smode = value;
+                    vmode = smode.GetEntryViewMode();
+                    updateSignal = Signal.UpdateScreen;
+                }
+            }
+        }
+
+        public enum Signal
+        {
+            Stay,
+            UpdateView,
+            UpdateScreen
+        }
+
+        public static Signal updateSignal = Signal.Stay;
+        public static ViewMode nextVMode = ViewMode.Dummy;
+
+        public static void UpdateView(ViewMode vmode)
+        {
+            VMode = vmode;
+        }
+
+        public static void UpdateScreen(ScreenMode smode)
+        {
+            SMode = smode;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
