be9d89532dde8bfff62a07451123fa5d3a0d25b3
miki <government.ocean@gmail.com>
2021-08-05 15:28:40 +0900

    遷移管理スクリプトの改修

--- Assets/Scripts/General/StageSelectManager.cs
+++ Assets/Scripts/General/StageSelectManager.cs
@@ -3,11 +3,8 @@ using UnityEngine;
 
 namespace General
 {
-    public class StageSelectManager : MonoBehaviour
+    public class StageSelectManager : ScreenManager
     {
-        public GameObject[] views;
-        public ParamBridge bridge;
-        // Start is called before the first frame update
         void Start()
         {
             if (ParamBridge.SMode == ScreenMode.Dummy)
@@ -20,40 +17,5 @@ namespace General
             }
             ParamBridge.UpdateSignal = ParamBridge.Signal.Stay;
         }
-
-        // Update is called once per frame
-        void Update()
-        {
-            switch (ParamBridge.UpdateSignal)
-            {
-                case ParamBridge.Signal.Stay:
-                    break;
-                case ParamBridge.Signal.UpdateView:
-                    SwitchView();
-                    break;
-                case ParamBridge.Signal.UpdateScreen:
-                    SwitchScreen();
-                    break;
-            }
-            // Debug.Log($"prev: {ParamBridge.PrevVMode.ToStringQuickly()}, curr: {ParamBridge.VMode.ToStringQuickly()}, signal: {ParamBridge.UpdateSignal}");
-        }
-
-        public void SwitchView()
-        {
-            var curr = ParamBridge.PrevVMode;
-            var next = ParamBridge.VMode;
-            Array.Find(views, v => v.name == next.ToStringQuickly()).SetActive(true);
-            Array.Find(views, v => v.name == curr.ToStringQuickly()).SetActive(false);
-            ParamBridge.UpdateSignal = ParamBridge.Signal.Stay;
-        }
-
-        public void SwitchScreen()
-        {
-            var sm = ParamBridge.SMode;
-            FadeManager.Instance.LoadScene(sm.ToStringQuickly(), 1.0f);
-            Debug.Log($"changed to {sm}");
-            ParamBridge.UpdateSignal = ParamBridge.Signal.Stay;
-        }
-        
     }
 }
