5b97f964ec209221497373ef0fc0bdf60ba9cbc2
harade <harade07@gmail.com>
2021-08-09 17:30:31 +0900

    Script"Catch" is uploaded.

---
+++ Assets/Scripts/Player/Catch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class Catch : MonoBehaviour
+    {
+
+        public GameManager gm;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (gm == null) GameObject.Find("GameManager").GetComponent<GameManager>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            if(collision.gameObject.CompareTag("Player")) gm.instance.Catched = true;
+        }
+    }
+}
