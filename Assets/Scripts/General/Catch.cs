9ba1e78bd164ad8b7b598dc17db234548b96dd6f
harade <harade07@gmail.com>
2021-08-09 20:20:13 +0900

    Scripts"Catch" and "Enemy Rin3" is Uploaded!!

---
+++ Assets/Scripts/General/Catch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace General
+{
+    public class Catch : MonoBehaviour
+    {
+
+        //public GameManager gm;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //if (gm == null) GameObject.Find("GameManager").GetComponent<GameManager>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+
+        void OnTriggerEnter(Collider collider)
+        {
+            if (collider.gameObject.CompareTag("Player"))
+            {
+                GameManager.Instance.Catched = true;
+                //Debug.Log("捕まえましてん");
+            }
+        }
+    }
+}
