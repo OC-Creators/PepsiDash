fd5a1ce5c0a16f36b5e2c819887ec517677253a7
miki@home <government.ocean@gmail.com>
2021-08-09 21:21:33 +0900

    Enemy Rinに捕まるとゲームオーバーになるよう修正

--- Assets/Scripts/Player/Catch.cs
+++ Assets/Scripts/Player/Catch.cs
@@ -6,24 +6,14 @@ namespace Player
 {
     public class Catch : MonoBehaviour
     {
-
-        public GameManager gm;
-
-        // Start is called before the first frame update
-        void Start()
+        void OnTriggerEnter(Collider collider)
         {
-            if (gm == null) GameObject.Find("GameManager").GetComponent<GameManager>();
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
-        void OnCollisionEnter(Collision collision)
-        {
-            if(collision.gameObject.CompareTag("Player")) gm.Instance.Catched = true;
+            if(collider.gameObject.CompareTag("Player"))
+            {
+                General.GameManager.Instance.Catched = true;
+                Debug.Log("catched!");
+            } 
+                
         }
     }
 }
